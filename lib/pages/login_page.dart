5521c1976282a4ab1a633ebae6cc8ffbf60b6439
rahull0328 <rmmehta2004@gmail.com>
2025-03-14 15:55:22 +0530

    completed login page UI along with components

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,38 +1,105 @@
+import 'package:crud/components/my_button.dart';
+import 'package:crud/components/my_textfield.dart';
 import 'package:flutter/material.dart';
 
-void main () {
-  runApp(const LoginPage());
-}
-
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+
+  //text editing controller
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  //login method
+  void login() {
+    
+  }
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //logo
-            Icon(
-              Icons.person,
-              size: 80,
-              color: Theme.of(context).colorScheme.inversePrimary,
-            ),
-            const SizedBox(height: 20),
-            //app name
-            Text(
-              "CRUD APP",
-              style: TextStyle(fontSize: 20),
-            ),
-          ],
-          //email textField
-          //password textField
-          //forgot password
-          //sign in button
-          //don't have an account register here
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Icon(
+                Icons.person,
+                size: 80,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              const SizedBox(height: 25),
+
+              //app name
+              const Text(
+                "NotesNest",
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 50),
+
+              //email textField
+              MyTextfield(
+                  hintText: "Email",
+                  obscureText: false,
+                  controller: emailController
+              ),
+              const SizedBox(height: 10),
+
+              //password textField
+              MyTextfield(
+                  hintText: "Password",
+                  obscureText: true,
+                  controller: passwordController
+              ),
+              const SizedBox(height: 10),
+
+              //forgot password
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "Forgot Password?",
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 25),
+
+              //sign in button
+              MyButton(
+                  text: "Login",
+                  onTap: login,
+              ),
+              const SizedBox(height: 25,),
+
+              //don't have an account register here
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an Account ?",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: const Text(
+                      " Register Here...",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
